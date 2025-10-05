56360b6407480a92ad07c448d2467721b74c1670
Parsh <parsh.cosmos11@gmail.com>
2025-10-05 12:42:08 +0200

    chore: update ESLint configuration for PoC
    - Disable prettier/prettier formatting checks
    - Disable max-len line length restrictions
    - Allow console.log statements for debugging
    - Focus on functionality over cosmetic issues
    - Document pragmatic approach for PoC phase

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    // Disable problematic prettier rules for this PoC
+    'prettier/prettier': 'off',
+    // Allow longer lines
+    'max-len': 'off',
+    // Allow console statements
+    'no-console': 'off',
+  },
 };
